ec0a91343a70931ec6a9b4401730a90cbad5889a
Cam <cam@someemailthatdoesntwork.com>
2011-07-03 21:48:42 +0100

    added simple validation to the upload form

--- app/controllers/UploadCodeReviewCommand.java
+++ app/controllers/UploadCodeReviewCommand.java
@@ -21,9 +21,14 @@ import com.google.gson.reflect.TypeToken;
 public class UploadCodeReviewCommand extends Controller {
 
 	 public static void upload(String name, String json, String thingsILike, String thingsIDontLike) {
+		 	validation.required(name);
+		 	validation.max("name", name.length(), 20);
+		 
 	    	JsonElement jsonObject = new JsonParser().parse(json);
 	    	Type type = new TypeToken<List<String>>(){}.getType();
 	    	List<String> lines = new Gson().fromJson(jsonObject, type);
+	    	validation.minSize("code", lines.size(), 1);
+	    	validation.isTrue("code", StringUtils.isNotBlank(StringUtils.join(lines, ""))).message("you must have some code");
 	    	
 	    	Type anotherType = new TypeToken<List<ReviewComment>>() {}.getType();
 	    	JsonElement anotherJsonObject = new JsonParser().parse(thingsILike);
@@ -32,10 +37,17 @@ public class UploadCodeReviewCommand extends Controller {
 	    	Type yetAnotherType = new TypeToken<List<ReviewComment>>() {}.getType();
 	    	JsonElement yetAnotherJsonObject = new JsonParser().parse(thingsIDontLike);
 	    	List<ReviewComment> yeahThingsIDontLike = new Gson().fromJson(yetAnotherJsonObject, yetAnotherType);
+	    	validation.isTrue("reviewComments", !yeahThingsILike.isEmpty() || !yeahThingsIDontLike.isEmpty()).message("you must have a least one review comment");
 	    	
-	    	CodeReview codeReview = new CodeReview(name, StringUtils.join(lines, "\n"), yeahThingsILike, yeahThingsIDontLike);
-	    	new CodeReviewRepository().save(codeReview);
+	    	if (validation.hasErrors()) {
+	    		params.flash();
+	    		validation.keep();
+	    		renderTemplate("UploadCodeReviewCommand/redirect-to-upload-form.json");
+	    	}
 
-	    	render(codeReview.getName());
-	    }
+    		CodeReview codeReview = new CodeReview(name, StringUtils.join(lines, "\n"), yeahThingsILike, yeahThingsIDontLike);
+    		new CodeReviewRepository().save(codeReview);
+	    	
+    		renderTemplate("UploadCodeReviewCommand/redirect-to-code-review.json", codeReview.getName());
+		}
 }
